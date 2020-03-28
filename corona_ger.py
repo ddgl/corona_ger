cdef9d3414e25538ad02af75b98cf2aed6ed792d
ddiege2s <dennis.diegel@smail.inf.h-brs.de>
2020-03-28 20:56:06 +0100

    total cases in dataframe

--- corona_ger.py
+++ corona_ger.py
@@ -1,6 +1,7 @@
 import requests
 import lxml.html as lh
 import pandas as pd
+import json
 
 url='https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahlen.html'
 
@@ -10,13 +11,23 @@ page = requests.get(url) #Response object called page containing bytes, handle f
 doc = lh.fromstring(page.content) #parses the string to a correct HTML document with <html> as parent node, doc is a lxml.html.HtmlElement object
 #print (type(doc))
 
-tr_elements = doc.xpath('//tr') #tr_elements is a list of all tr-elements (//) in doc (which also are HtmlElements)
+tr_elements = doc.xpath('//tbody/tr') #tr_elements is a list of all tr-elements (//) which are children of tbody in doc (which also are HtmlElements)
 #print (type(tr_elements))
 
-#check content and size of tr_elements in order to check if correct elements where selected
+""" #check content and size of tr_elements in order to check if correct elements where selected
 #print (tr_elements)
 #print (len(tr_elements))
 for T in tr_elements:
     #print (len(T))
     #print (type(T))
-    print(T.text_content())
+    print(T[0].text_content(), T[1].text_content()) """
+
+total_cases = {}
+for i in tr_elements:
+    total_cases[i[0].text_content()] = i[1].text_content()
+
+""" for x in total_cases:
+    print(x, total_cases[x]) """
+
+df = pd.DataFrame([total_cases])
+print(df)
